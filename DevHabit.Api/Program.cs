3bdd7ce9f52d4525c9482eef86c4ac7d99602b4d
omlnaut <oneironaut.oml@gmail.com>
2025-02-26 14:57:50 +0100

    switched to controller template

--- DevHabit.Api/Program.cs
+++ DevHabit.Api/Program.cs
@@ -1,13 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,7 +14,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.MapControllers();
 
 app.Run();
-
