2661fd237e491ec037fc49f86134c0872a10cc6b
omlnaut <oneironaut.oml@gmail.com>
2025-02-27 22:04:21 +0100

    simplify name

--- DevHabit/DevHabit.Api/Database/Configurations/HabitConfiguration.cs
+++ DevHabit/DevHabit.Api/Database/Configurations/HabitConfiguration.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DevHabit.Api.Database.Configurations;
 
-public sealed class HabitConfiguration : IEntityTypeConfiguration<Entities.Habit>
+public sealed class HabitConfiguration : IEntityTypeConfiguration<Habit>
 {
     public void Configure(EntityTypeBuilder<Habit> builder)
     {
@@ -15,7 +15,7 @@ public sealed class HabitConfiguration : IEntityTypeConfiguration<Entities.Habit
         builder.Property(h => h.Name).HasMaxLength(500);
         builder.Property(h => h.Description).HasMaxLength(500);
 
-        // Todo: what does ownsOne do here?
+        // later: what does ownsOne do here?
         builder.OwnsOne(h => h.Frequency);
 
         builder.OwnsOne(h => h.Target, t => t.Property(t => t.Unit).HasMaxLength(50));
