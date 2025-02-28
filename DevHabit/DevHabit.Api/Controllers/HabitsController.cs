16fd68b28f9550f43e7ec243408de7ce49f088ba
omlnaut <oneironaut.oml@gmail.com>
2025-02-28 13:54:46 +0100

    habits dto

--- DevHabit/DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -1,4 +1,6 @@
 using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.Entities;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,11 +12,42 @@ namespace DevHabit.Api.Controllers;
 public class HabitsController(ApplicationDbContext dbContext) : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetHabits()
+    public async Task<ActionResult<HabitsCollectionDto>> GetHabits()
     {
-        var habits = await dbContext.Habits.ToListAsync();
+        List<HabitDto> habits = await dbContext
+            .Habits
+            .Select(habit => new HabitDto
+            {
+                Id = habit.Id,
+                Name = habit.Name,
+                Description = habit.Description,
+                Frequency = new FrequencyDto
+                {
+                    TimesPerPeriod = habit.Frequency.TimesPerPeriod,
+                    Type = habit.Frequency.Type
+                },
+                Target = new TargetDto
+                {
+                    Value = habit.Target.Value,
+                    Unit = habit.Target.Unit
+                },
+                Status = habit.Status,
+                IsArchived = habit.IsArchived,
+                EndDate = habit.EndDate,
+                Milestone = habit.Milestone == null ? null : new MilestoneDto
+                {
+                    Target = habit.Milestone.Target,
+                    Current = habit.Milestone.Current
+                },
+                CreatedAtUtc = habit.CreatedAtUtc,
+                UpdatedAtUtc = habit.UpdatedAtUtc,
+                LastCompletedAt = habit.LastCompletedAt
+            })
+            .ToListAsync();
 
-        return Ok(habits);
+        var habitsDto = new HabitsCollectionDto { Data = habits };
+
+        return Ok(habitsDto);
     }
 
 }
--- DevHabit/DevHabit.Api/DTOs/Habits/HabitDto.cs
+++ DevHabit/DevHabit.Api/DTOs/Habits/HabitDto.cs
@@ -1,20 +1,26 @@
 using HabitStatus = DevHabit.Api.Entities.HabitStatus;
 using FrequencyType = DevHabit.Api.Entities.FrequencyType;
+using System.Collections.ObjectModel;
 
 namespace DevHabit.Api.DTOs.Habits;
+
+public sealed record HabitsCollectionDto
+{
+    public required List<HabitDto> Data { get; init; }
+}
 
 public sealed record HabitDto
 {
     public required string Id { get; init; }
-    public string Name { get; init; } = string.Empty;
+    public required string Name { get; init; } = string.Empty;
     public string? Description { get; init; }
     public required FrequencyDto Frequency { get; init; }
     public required TargetDto Target { get; init; }
     public HabitStatus Status { get; init; }
-    public bool IsArchived { get; init; }
+    public required bool IsArchived { get; init; }
     public DateOnly? EndDate { get; init; }
     public MilestoneDto? Milestone { get; init; }
-    public DateTime CreatedAtUtc { get; init; }
+    public required DateTime CreatedAtUtc { get; init; }
     public DateTime? UpdatedAtUtc { get; init; }
     public DateTime? LastCompletedAt { get; init; }
 
@@ -22,17 +28,17 @@ public sealed record HabitDto
 
 public sealed record FrequencyDto
 {
-    public int TimesPerPeriod { get; init; }
+    public required int TimesPerPeriod { get; init; }
 
-    public FrequencyType Type { get; init; }
+    public required FrequencyType Type { get; init; }
 }
 public sealed record TargetDto
 {
-    public int Value { get; init; }
+    public required int Value { get; init; }
     public required string Unit { get; init; }
 }
 public sealed record MilestoneDto
 {
-    public int Target { get; init; }
-    public int Current { get; init; }
+    public required int Target { get; init; }
+    public required int Current { get; init; }
 }
