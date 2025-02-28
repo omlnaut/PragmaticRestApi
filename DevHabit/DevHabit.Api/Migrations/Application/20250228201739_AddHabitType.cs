5b7e74b9c518499ff3206f1378c779343c6008da
omlnaut <oneironaut.oml@gmail.com>
2025-02-28 21:20:00 +0100

    AddHabitType migration

---
+++ DevHabit/DevHabit.Api/Migrations/Application/20250228201739_AddHabitType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevHabit.Api.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class AddHabitType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                schema: "dev_habit",
+                table: "habits",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                schema: "dev_habit",
+                table: "habits");
+        }
+    }
+}
