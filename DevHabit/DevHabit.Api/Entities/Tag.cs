f3fc887136a68bf50f4e66fe98de0f8ed3f38657
olivendor <oliver.rueger@relaxdays.de>
2025-03-05 13:17:22 +0000

    db config

--- DevHabit/DevHabit.Api/Entities/Tag.cs
+++ DevHabit/DevHabit.Api/Entities/Tag.cs
@@ -2,7 +2,7 @@ namespace DevHabit.Api.Entities;
 
 public sealed class Tag
 {
-    public string Id { get; set; }
+    public required string Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     public DateTime CreatedAtUtc { get; set; }
