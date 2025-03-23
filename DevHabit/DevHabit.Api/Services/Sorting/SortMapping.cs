c8a096e8b71b7bcbd473dd7eb84b465c5d51c7c8
olivendor <oliver.rueger@relaxdays.de>
2025-03-23 20:21:20 +0000

    ApplySort extension

--- DevHabit/DevHabit.Api/Services/Sorting/SortMapping.cs
+++ DevHabit/DevHabit.Api/Services/Sorting/SortMapping.cs
@@ -2,6 +2,8 @@
 #pragma warning disable S2326 // Unused type parameters should be removed
 #pragma warning disable CA1819 // Properties should not return arrays
 
+using System.Linq.Dynamic.Core;
+
 namespace DevHabit.Api.Services.Sorting;
 
 public sealed record SortMapping(string SortField, string PropertyName, bool Reverse = false);
@@ -30,5 +32,71 @@ public sealed class SortMappingProvider(IEnumerable<ISortMappingDefinition> sort
         }
 
         return sortMappingDefinition.Mappings;
+    }
+}
+
+internal static class QueryableExtensions
+{
+    public static IQueryable<T> ApplySort<T>(this IQueryable<T> query,
+                                             string? sort,
+                                             SortMapping[] mappings,
+                                             string defaultOrderBy = "Id")
+    {
+        if (string.IsNullOrWhiteSpace(sort))
+        {
+            return query.OrderBy(defaultOrderBy);
+        }
+
+        var sortFields = sort.Split(',')
+                             .Select(s => s.Trim())
+                             .Where(s => !string.IsNullOrWhiteSpace(s))
+                             .ToArray();
+
+        var orderByParts = new List<string>();
+
+        foreach (var sortField in sortFields)
+        {
+            var (name, isDescending) = ParseSortField(sortField);
+
+            var mapping = mappings.First(m => m.SortField == name);
+
+            var direction = (mapping.Reverse, isDescending) switch
+            {
+                (true, true) => "asc",
+                (true, false) => "desc",
+                (false, true) => "desc",
+                (false, false) => "asc",
+            };
+
+            var fieldString = $"{name} {direction}";
+            orderByParts.Add(fieldString);
+        }
+
+        var orderBy = string.Join(",", orderByParts);
+
+        return query.OrderBy(orderBy);
+    }
+
+    /// <summary>
+    /// field is of type "name" or "name asc" or "name desc". If not given, descending by default
+    /// </summary>
+    /// <param name="field"></param>
+    /// <returns></returns>
+    private static (string SortField, bool IsDescending) ParseSortField(string field)
+    {
+        var parts = field.Split(" ");
+        var name = parts[0];
+
+        if (parts.Length == 1)
+        {
+            return (name, true);
+        }
+
+        var sortType = parts[1];
+
+        var isDescending = sortType.Equals("desc", StringComparison.OrdinalIgnoreCase);
+
+        return (name, isDescending);
+
     }
 }
