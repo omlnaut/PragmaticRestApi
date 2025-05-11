22a80269f915acfec3ae629e88d254f2d413be64
omlnaut <oneironaut.oml@gmail.com>
2025-05-11 19:26:12 +0000

    UserDto

--- DevHabit/DevHabit.Api/DTOs/Users/UsersDto.cs
+++ DevHabit/DevHabit.Api/DTOs/Users/UsersDto.cs
@@ -1,11 +1,10 @@
-namespace DevHabit.Api.Entities;
+namespace DevHabit.Api.DTOs.Users;
 
-public sealed class User
+public sealed record UsersDto
 {
     public required string Id { get; set; }
     public required string Mail { get; set; }
     public required string Name { get; set; }
     public required DateTime CreatedAtUtc { get; set; }
-    public DateTime? UpdatedAtUtc { get; set; }
-    public required string IdentityId { get; set; }
+    public required DateTime UpdatedAtUtc { get; set; }
 }
