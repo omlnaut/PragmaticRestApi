c441826162694ea109b947ccb38fef7f1b9f1d55
omlnaut <oneironaut.oml@gmail.com>
2025-05-17 18:29:08 +0000

    create pending user migration

---
+++ DevHabit/DevHabit.Api/Migrations/Application/20250517182806_AddUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevHabit.Api.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class AddUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "dev_habit",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    mail = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    identity_id = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_identity_id",
+                schema: "dev_habit",
+                table: "users",
+                column: "identity_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_mail",
+                schema: "dev_habit",
+                table: "users",
+                column: "mail",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "dev_habit");
+        }
+    }
+}
