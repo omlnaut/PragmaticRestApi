75eca70eb8dd3798d65ceef7b4be0ecafb5d5649
omlnaut <oneironaut.oml@gmail.com>
2025-05-26 09:11:09 +0000

    refresh token entity

---
+++ DevHabit/DevHabit.Api/Entities/RefreshToken.cs
@@ -0,0 +1,9 @@
+namespace DevHabit.Api.Entities;
+
+public sealed class RefreshToken
+{
+    public Guid Id { get; set; }
+    public required string UserId { get; set; }
+    public required string Token { get; set; }
+    public required DateTime ExpiresAtUtc { get; set; }
+}
