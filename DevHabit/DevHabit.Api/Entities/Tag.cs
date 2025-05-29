24da2db4ac9ad4691560d4f73f0c5871fbbd7194
omlnaut <oneironaut.oml@gmail.com>
2025-05-29 19:03:02 +0000

    tag user

--- DevHabit/DevHabit.Api/Entities/Tag.cs
+++ DevHabit/DevHabit.Api/Entities/Tag.cs
@@ -3,6 +3,7 @@ namespace DevHabit.Api.Entities;
 public sealed class Tag
 {
     public required string Id { get; set; }
+    public required string UserId { get; set; }
 
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
