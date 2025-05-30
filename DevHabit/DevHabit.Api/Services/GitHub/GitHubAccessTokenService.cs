3302500f8834e49cc4211b8db5314f30082f7e92
omlnaut <oneironaut.oml@gmail.com>
2025-05-30 22:38:10 +0200

    StoreAsync

---
+++ DevHabit/DevHabit.Api/Services/GitHub/GitHubAccessTokenService.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DevHabit.Api.Database;
+using DevHabit.Api.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Services.GitHub;
+
+public class GitHubAccessTokenService(ApplicationDbContext applicationDbContext)
+{
+    public async Task StoreAsync(string userId, StoreGithubAccessTokenDto dto)
+    {
+        var dbToken = await GetToken(userId);
+
+        if (dbToken is null)
+        {
+            var newToken = new GitHubAccessToken()
+            {
+                Id = $"gh_{Guid.NewGuid()}",
+                UserId = userId,
+                Token = dto.Token,
+                ExpiresAtUtc = DateTime.UtcNow.AddDays(dto.ExpiresInDays),
+                CreatedAtUtc = DateTime.UtcNow
+            };
+            applicationDbContext.GitHubAccessTokens.Add(newToken);
+        }
+        else
+        {
+            dbToken.Token = dto.Token;
+            dbToken.ExpiresAtUtc = DateTime.UtcNow.AddDays(dto.ExpiresInDays);
+            dbToken.CreatedAtUtc = DateTime.UtcNow;
+        }
+
+        await applicationDbContext.SaveChangesAsync();
+    }
+
+    private async Task<GitHubAccessToken?> GetToken(string userId)
+    {
+        return await applicationDbContext
+            .GitHubAccessTokens
+            .FirstOrDefaultAsync(gh => gh.UserId == userId);
+    }
+
+}
+
+public sealed record StoreGithubAccessTokenDto(string Token, int ExpiresInDays);
